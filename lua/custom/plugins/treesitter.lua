61439fc2fe0d20878f8dd8d5fbda1300155f50bd
David <davidmontiel549@gmail.com>
2026-01-09 15:28:39 -0800

    Add Languages to TS

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,51 +1,55 @@
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    event = { 'BufReadPost', 'BufNewFile' },
-    -- This is the critical change: Use opts instead of manual require
-    opts = {
-      ensure_installed = {
-        -- Base & Shell
-        'bash',
-        'fish',
-        'make',
-        'cmake',
-        'toml',
-        'yaml',
-        'printf',
-        'diff',
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        event = { 'BufReadPost', 'BufNewFile' },
+        -- This is the critical change: Use opts instead of manual require
+        opts = {
+            ensure_installed = {
+                -- Base & Shell
+                'bash',
+                'fish',
+                'make',
+                'cmake',
+                'toml',
+                'yaml',
+                'printf',
+                'diff',
 
-        -- C / C++ / Rust
-        'c',
-        'cpp',
-        'rust',
+                -- C / C++ / Rust
+                'c',
+                'cpp',
+                'rust',
 
-        -- Go
-        'go',
-        'gomod',
-        'gosum',
-        'gotmpl',
-        'gowork',
+                -- Go
+                'go',
+                'gomod',
+                'gosum',
+                'gotmpl',
+                'gowork',
 
-        -- Web & Docs
-        'html',
-        'markdown',
-        'markdown_inline',
-        'query',
+                -- Web & Docs
+                'html',
+                'markdown',
+                'markdown_inline',
+                'query',
+                'javascript',
+                'typescript',
+                'jsx',
+                'tsx',
 
-        -- Neovim / Lua
-        'lua',
-        'luadoc',
-        'vim',
-        'vimdoc',
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        use_languagetree = true,
-      },
-      indent = { enable = true },
+                -- Neovim / Lua
+                'lua',
+                'luadoc',
+                'vim',
+                'vimdoc',
+            },
+            auto_install = true,
+            highlight = {
+                enable = true,
+                use_languagetree = true,
+            },
+            indent = { enable = true },
+        },
     },
-  },
 }
