d49f79a151a351d8a678b19da5a707e51beee817
David <davidmontiel549@gmail.com>
2026-01-21 20:17:47 -0800

    add blink compat

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -33,6 +33,15 @@ return {
     --
     { import = "nvchad.blink.lazyspec" },
     {
+        "saghen/blink.compat",
+        -- use v2.* for blink.cmp v1.*
+        version = "2.*",
+        -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+        lazy = true,
+        -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+        opts = {},
+    },
+    {
         "Saghen/blink.cmp",
         opts = require "configs.blink",
     },
